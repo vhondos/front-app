b4f279cfd155c85aa648e7ba0fcdcc69d213b4e4
V4l3ntino <valentinohontar15@gmail.com>
2024-12-05 00:11:08 +0100

    first commit

---
+++ app/dashboard/page.tsx
@@ -0,0 +1,116 @@
+const Dashboard = () => {
+    return (
+        <div className='main-container w-[393px] h-[852px] bg-[#f3f3f3] relative overflow-hidden mx-auto my-0'>
+            <div className='w-[326px] h-[700px] relative overflow-hidden shadow-[5px_5px_13px_0_rgba(69,69,72,0.9)] z-[5] mt-[49px] mr-0 mb-0 ml-[33px]'>
+                <div className='w-[313px] h-[18px] relative z-[49] mt-[5px] mr-0 mb-0 ml-[9px]'>
+                    <div className='w-[16px] h-[16px] absolute top-0 left-[297px] overflow-hidden z-[12]' />
+                    <div className='w-[4.15%] h-[72.22%] absolute top-[27.78%] left-0 z-[49]'>
+                        <div className='w-full h-full bg-[url(../assets/images/da6ad8c5-4a09-4f4a-b595-04b3e5456e84.png)] bg-[length:100%_100%] bg-no-repeat absolute top-0 left-0 z-50' />
+                    </div>
+                    <div className='w-[3.83%] h-[66.67%] bg-[url(../assets/images/1d9e9cf4-a000-4039-a87b-8cc08c427544.png)] bg-[length:100%_100%] bg-no-repeat absolute top-[27.78%] left-[93.61%] z-[13]' />
+                </div>
+                <div className='flex w-[249px] pt-[6px] pr-[15px] pb-[6px] pl-[15px] justify-between items-center flex-nowrap bg-[#fff] rounded-[21px] relative overflow-hidden z-[43] mt-[-3px] mr-0 mb-0 ml-[38px]'>
+                    <input className='w-[249px] h-[28px] shrink-0 bg-transparent border-none absolute top-0 left-0 z-[48]' />
+                    <span className="h-[15px] shrink-0 basis-auto font-['Inter'] text-[12px] font-normal leading-[14.523px] text-[#000] relative text-left whitespace-nowrap z-[44]">
+                        Seach...
+                    </span>
+                    <div className='w-[14px] h-[14px] shrink-0 relative z-[45]' />
+                    <div className='w-[16px] h-[16px] shrink-0 relative z-[46]'>
+                        <div className='w-[12.86px] h-[12.86px] bg-[url(../assets/images/9068cb9f-51f2-4ae5-8762-08be2a0601a7.png)] bg-[length:100%_100%] bg-no-repeat relative z-[47] mt-[1.5px] mr-0 mb-0 ml-[1.64px]' />
+                    </div>
+                </div>
+                <div className='flex w-[306px] flex-col gap-[14px] items-end flex-nowrap relative z-[14] mt-[26px] mr-0 mb-0 ml-[9px]'>
+                    <div className='flex pt-[15px] pr-[3px] pb-[15px] pl-[9px] gap-[164px] items-start self-stretch shrink-0 flex-nowrap bg-[rgba(255,251,251,0.9)] relative overflow-hidden z-[15]'>
+                        <div className='flex w-[96px] flex-col gap-[-5px] justify-center items-start shrink-0 flex-nowrap relative z-[16]'>
+                            <span className="h-[21px] self-stretch shrink-0 basis-auto font-['Inter'] text-[12px] font-normal leading-[14.523px] text-[#000] relative text-left whitespace-nowrap z-[17]">
+                                Fenando Tejero
+                            </span>
+                            <span className="flex w-[60px] h-[8px] justify-start items-start shrink-0 basis-auto font-['Inter'] text-[6px] font-normal leading-[7.261px] text-[#000] relative text-left whitespace-nowrap z-[18]">
+                                12-02-2023 / 13:03
+                            </span>
+                        </div>
+                        <div className='w-[24px] h-[24px] shrink-0 relative z-[19]'>
+                            <div className='flex w-[24px] h-[23px] pt-[9px] pr-[10px] pb-[9px] pl-[10px] gap-[10px] justify-center items-center flex-nowrap bg-[#fff] rounded-[26px] relative overflow-hidden z-20 mt-px mr-0 mb-0 ml-[-3px]'>
+                                <span className="flex w-[16px] h-[16px] justify-start items-start shrink-0 basis-auto font-['Inter'] text-[12px] font-normal leading-[14.523px] text-[#000] relative text-left whitespace-nowrap z-[21]">
+                                    -6
+                                </span>
+                            </div>
+                        </div>
+                    </div>
+                    <div className='flex pt-[15px] pr-[3px] pb-[15px] pl-[9px] gap-[164px] items-start self-stretch shrink-0 flex-nowrap bg-[rgba(255,251,251,0.9)] relative overflow-hidden z-[22]'>
+                        <div className='flex w-[96px] flex-col gap-[-5px] justify-center items-start shrink-0 flex-nowrap relative z-[23]'>
+                            <span className="h-[21px] self-stretch shrink-0 basis-auto font-['Inter'] text-[12px] font-normal leading-[14.523px] text-[#000] relative text-left whitespace-nowrap z-[24]">
+                                Fenando Tejero
+                            </span>
+                            <span className="flex w-[60px] h-[8px] justify-start items-start shrink-0 basis-auto font-['Inter'] text-[6px] font-normal leading-[7.261px] text-[#000] relative text-left whitespace-nowrap z-[25]">
+                                12-02-2023 / 13:03
+                            </span>
+                        </div>
+                        <div className='w-[24px] h-[24px] shrink-0 relative z-[26]'>
+                            <div className='flex w-[24px] h-[23px] pt-[9px] pr-[10px] pb-[9px] pl-[10px] gap-[10px] justify-center items-center flex-nowrap bg-[#fff] rounded-[26px] relative overflow-hidden z-[27] mt-px mr-0 mb-0 ml-[-3px]'>
+                                <span className="flex w-[16px] h-[16px] justify-start items-start shrink-0 basis-auto font-['Inter'] text-[12px] font-normal leading-[14.523px] text-[#000] relative text-left whitespace-nowrap z-[28]">
+                                    -6
+                                </span>
+                            </div>
+                        </div>
+                    </div>
+                    <div className='flex pt-[15px] pr-[3px] pb-[15px] pl-[9px] gap-[164px] items-start self-stretch shrink-0 flex-nowrap bg-[rgba(255,251,251,0.9)] relative overflow-hidden z-[29]'>
+                        <div className='flex w-[96px] flex-col gap-[-5px] justify-center items-start shrink-0 flex-nowrap relative z-30'>
+                            <span className="h-[21px] self-stretch shrink-0 basis-auto font-['Inter'] text-[12px] font-normal leading-[14.523px] text-[#000] relative text-left whitespace-nowrap z-[31]">
+                                Fenando Tejero
+                            </span>
+                            <span className="flex w-[60px] h-[8px] justify-start items-start shrink-0 basis-auto font-['Inter'] text-[6px] font-normal leading-[7.261px] text-[#000] relative text-left whitespace-nowrap z-[32]">
+                                12-02-2023 / 13:03
+                            </span>
+                        </div>
+                        <div className='w-[24px] h-[24px] shrink-0 relative z-[33]'>
+                            <div className='flex w-[24px] h-[23px] pt-[9px] pr-[10px] pb-[9px] pl-[10px] gap-[10px] justify-center items-center flex-nowrap bg-[#fff] rounded-[26px] relative overflow-hidden z-[34] mt-px mr-0 mb-0 ml-[-3px]'>
+                                <span className="flex w-[16px] h-[16px] justify-start items-start shrink-0 basis-auto font-['Inter'] text-[12px] font-normal leading-[14.523px] text-[#000] relative text-left whitespace-nowrap z-[35]">
+                                    -6
+                                </span>
+                            </div>
+                        </div>
+                    </div>
+                    <div className='flex pt-[15px] pr-[3px] pb-[15px] pl-[9px] gap-[164px] items-start self-stretch shrink-0 flex-nowrap bg-[rgba(255,251,251,0.9)] relative overflow-hidden z-[36]'>
+                        <div className='flex w-[96px] flex-col gap-[-5px] justify-center items-start shrink-0 flex-nowrap relative z-[37]'>
+                            <span className="h-[21px] self-stretch shrink-0 basis-auto font-['Inter'] text-[12px] font-normal leading-[14.523px] text-[#000] relative text-left whitespace-nowrap z-[38]">
+                                Fenando Tejero
+                            </span>
+                            <span className="flex w-[60px] h-[8px] justify-start items-start shrink-0 basis-auto font-['Inter'] text-[6px] font-normal leading-[7.261px] text-[#000] relative text-left whitespace-nowrap z-[39]">
+                                12-02-2023 / 13:03
+                            </span>
+                        </div>
+                        <div className='w-[24px] h-[24px] shrink-0 relative z-40'>
+                            <div className='flex w-[24px] h-[23px] pt-[9px] pr-[10px] pb-[9px] pl-[10px] gap-[10px] justify-center items-center flex-nowrap bg-[#fff] rounded-[26px] relative overflow-hidden z-[41] mt-px mr-0 mb-0 ml-[-3px]'>
+                                <span className="flex w-[16px] h-[16px] justify-start items-start shrink-0 basis-auto font-['Inter'] text-[12px] font-normal leading-[14.523px] text-[#000] relative text-left whitespace-nowrap z-[42]">
+                                    -6
+                                </span>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div className='flex w-[326px] pt-[10px] pr-[10px] pb-[10px] pl-[10px] gap-[10px] justify-center items-center flex-nowrap border-solid border-t border-t-[#000] relative shadow-[0_4px_4px_0_rgba(0,0,0,0.25)] z-[6] mt-[304px] mr-0 mb-0 ml-0'>
+                    <div className='w-[24px] h-[24px] shrink-0 relative z-[7]'>
+                        <div className='w-full h-full bg-[url(../assets/images/22bd8807-da7d-45aa-9a6e-ab05fa2c70ce.png)] bg-[length:100%_100%] bg-no-repeat absolute top-0 left-0 z-[8]' />
+                    </div>
+                    <span className="flex w-[35px] h-[18px] justify-start items-start shrink-0 basis-auto font-['Inter'] text-[12px] font-normal leading-[14.523px] text-[#000] relative text-left whitespace-nowrap z-[9]">
+                        Pag 1
+                    </span>
+                    <div className='w-[24px] h-[24px] shrink-0 relative overflow-hidden z-10'>
+                        <div className='w-full h-full bg-[url(../assets/images/4be7f28d-1b65-4b95-97f3-f842570ca4b6.png)] bg-[length:100%_100%] bg-no-repeat absolute top-0 left-0 z-[11]' />
+                    </div>
+                </div>
+            </div>
+            <div className='w-[393px] h-[93px] bg-[#131111] relative overflow-hidden mt-[30px] mr-0 mb-0 ml-0'>
+                <div className='w-[24px] h-[24px] absolute top-[35px] left-1/2 translate-x-[-52.08%] translate-y-0 z-[1]'>
+                    <div className='w-[204.17%] h-[208.33%] bg-[url(../assets/images/3b3c0dd1-0038-47d9-89f9-2f9071b83790.png)] bg-[length:100%_100%] bg-no-repeat absolute top-[-54.17%] left-[-50%] z-[2]' />
+                </div>
+                <div className='w-[24px] h-[24px] absolute top-[35px] left-[184px] z-[3]'>
+                    <div className='w-[18px] h-[18px] bg-[url(../assets/images/15b4b907-ba92-4ab0-846c-1133ea82de4c.png)] bg-[length:100%_100%] bg-no-repeat relative z-[4] mt-[3px] mr-0 mb-0 ml-[3px]' />
+                </div>
+            </div>
+        </div>
+    );
+}
+
+export default Dashboard;
